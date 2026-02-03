fixed scaling noise

src/waveform.py
@@ -3,6 +3,7 @@ import numpy.typing as npt
 
 from src.utils import freq_from_pitch
 
+
 class Noise:
     def __init__(
         self,
@@ -21,8 +22,7 @@ class Noise:
 
     def brown(self) -> npt.NDArray:
         return self.filter_noise(
-            lambda freq: 1
-            / np.where(freq == 0, float("inf"), freq)
+            lambda freq: 1 / np.where(freq == 0, float("inf"), freq)
         )
 
     def blue(self) -> npt.NDArray:
@@ -37,19 +37,19 @@ class Noise:
     def filter_noise(self, filter_func) -> npt.NDArray:
         noise = self.noise_func()
         noise_fft = np.fft.rfft(noise)
-        filtered_freq = filter_func(
-            np.fft.rfftfreq(noise.shape[0])
-        )
+        filtered_freq = filter_func(np.fft.rfftfreq(noise.shape[0]))
         # normalize filter to preserve power
-        filtered_freq /= filtered_freq.std()
-        filtered_noise = noise_fft * filtered_freq
-        return np.fft.irfft(filtered_noise)
+        filtered_noise_fft = noise_fft * filtered_freq
+        noise = np.fft.irfft(filtered_noise_fft)
+        noise /= noise.std()
+        return noise
 
 
 class NoiseSynth:
     """
     A simple noise synthesizer.
     """
+
     def __init__(
         self,
         *,