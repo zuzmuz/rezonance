46b96c03b9d9b54dc7e07b9e2f09ad02f68a2896
zuzmuz <hamadehzaher0@gmail.com>
2026-02-04 16:28:33 +0100

    fixed harm script

--- scripts/harm.py
+++ scripts/harm.py
@@ -1,6 +1,8 @@
 import torch
 import matplotlib.pyplot as plt
+from src.utils import freq_from_pitch
 from src.waveform import WaveformSynth
+
 
 def run(*args, **kwargs):
     sample_rate = 16_000.0
@@ -10,18 +12,21 @@ def run(*args, **kwargs):
     waveform_synth = WaveformSynth(
         sample_rate=sample_rate, buffer_size=buffer_size, A4=A4
     )
-    
-    waveform = waveform_synth.gen_poly(
-        torch.tensor([
+
+    data = torch.tensor(
+        [
             [[57, 0, 0.6], [69, 0.2, 0.3]],
             [[57, 0, 0.6], [69, 0.2, 0.0]],
-            [[57, 0, 0.0], [69, 0.2, 0.3]]
-        ])
+            [[57, 0, 0.0], [69, 0.2, 0.3]],
+        ]
     )
+
+    data[:, :, 0] = freq_from_pitch(data[:, :, 0], A4=A4)
+
+    waveform = waveform_synth.gen_poly(data)
 
     plt.figure()
     plt.plot(waveform[0].cpu().detach().numpy(), linewidth=0.5)
     plt.plot(waveform[1].cpu().detach().numpy(), linewidth=0.5)
     plt.plot(waveform[2].cpu().detach().numpy(), linewidth=0.5)
     plt.show()
-
