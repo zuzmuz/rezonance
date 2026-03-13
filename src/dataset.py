f370643f126f6bd5ae7333b70715fb5562a784b7
zuzmuz <hamadehzaher0@gmail.com>
2026-03-13 16:20:19 +0100

    created random dataset removed the lazy dataset

--- src/dataset.py
+++ src/dataset.py
@@ -9,7 +9,7 @@ from src.utils import (
     pitch_from_freq,
     current_device,
 )
-from src.waveform import WaveformSynth
+from src.waveform import Instrument, WaveformSynth
 from src.noise_generators import NoiseSynth
 
 logger = logging.getLogger(__name__)
@@ -121,91 +121,6 @@ class NoisySineWaveformDataset(Dataset):
         return waveform, pitch
 
 
-class LazyNoisySineWaveformDataset(Dataset):
-    def __init__(
-        self,
-        nb_pitches: int,
-        nb_phases: int,
-        /,
-        noises: list[NoiseSynth],
-        *,
-        sample_rate: Number = 16_000.0,
-        buffer_size: int = 1024,
-        A4: Number = 440.0,
-        min_pitch: Number = 20,
-        max_pitch: Number | None = None,
-        seed: int | None = None,
-    ):
-
-        if seed:
-            torch.manual_seed(seed)
-
-        self.synth = WaveformSynth(
-            sample_rate=sample_rate, buffer_size=buffer_size, A4=A4
-        )
-
-        max_possible_pitch = pitch_from_freq(
-            0.25 * sample_rate, A4=A4
-        )
-
-        if not max_pitch:
-            max_pitch = max_possible_pitch
-        elif max_pitch > max_possible_pitch:
-            logger.warning(
-                "Provided max_pitch %.2f "
-                "exceeds the maximum possible pitch %.2f"
-                "for the given sample rate %.2f.",
-                max_pitch,
-                max_possible_pitch,
-                sample_rate,
-            )
-
-        self.pitches = torch.linspace(
-            min_pitch, max_pitch, nb_pitches, dtype=torch.float32
-        )
-
-        self.freqencies = freq_from_pitch(self.pitches, A4=A4)
-
-        self.phases = torch.linspace(
-            -1, 2 - 1 / nb_phases, nb_phases, dtype=torch.float32
-        )
-
-        self.noises = torch.stack(
-            [synth(buffer_size) for synth in noises]
-        )
-
-        print(
-            f"{self.pitches.shape=} {self.phases.shape=} {self.noises.shape=}"
-        )
-
-    def __len__(self):
-        return (
-            self.pitches.size(0)
-            * self.phases.size(0)
-            * self.noises.size(0)
-        )
-
-    def __getitem__(self, idx):
-        pitch = self.pitches[
-            idx // (self.phases.size(0) * self.noises.size(0))
-        ]
-        freq = self.freqencies[
-            idx // (self.phases.size(0) * self.noises.size(0))
-        ]
-        phase = self.phases[
-            (idx // self.noises.size(0)) % self.phases.size(0)
-        ]
-        noise = self.noises[idx % self.noises.size(0)]
-
-        waveform = self.synth.gen_mono(
-            torch.tensor([freq, phase]).unsqueeze(0)
-        )
-
-        # print(f"sizes {waveform[0].shape}  pi {pitch.shape}")
-
-        return waveform[0] + noise, pitch
-
-
 class RandomTimbralDataSet(Dataset):
     def __init__(
         self,
@@ -227,8 +142,11 @@ class RandomTimbralDataSet(Dataset):
         self.sample_rate = sample_rate
         self.A4 = A4
 
-        self.synth = WaveformSynth(
-            sample_rate=sample_rate, buffer_size=buffer_size, A4=A4
+        self.instrument = Instrument.random(
+            1.5,
+            sample_rate=sample_rate,
+            buffer_size=buffer_size,
+            A4=A4,
         )
 
         max_possible_pitch = pitch_from_freq(
@@ -246,20 +164,28 @@ class RandomTimbralDataSet(Dataset):
                 max_possible_pitch,
                 sample_rate,
             )
-        
-        pitches = torch.linspace(
+
+        self.pitches = torch.linspace(
             min_pitch, max_pitch, nb_pitches, dtype=torch.float32
         )
 
-        ranks = get_rank_of_pitch(
-            pitches,
-            sample_rate=sample_rate,
-            A4=A4,
+        self.nb_harm_dist = nb_harm_dist
+
+        self.data = self.instrument.generate(
+            self.pitches,
+            per_pitch=nb_harm_dist,
         )
 
-        # rank_pitches[0] = min_pitch
-        # rank_pitches[-1] = max_pitch
+        self.data /= self.data.std(dim=1, keepdim=True)
 
-        
+        self.data.to(current_device)
 
+    def __len__(self):
+        return self.data.size(0)
 
+    def __getitem__(self, idx):
+        pitch = self.pitches[idx // self.nb_harm_dist]
+
+        waveform = self.data[idx]
+
+        return waveform, pitch
